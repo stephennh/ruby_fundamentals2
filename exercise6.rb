d2e96a427b4fa59cbcd7ae8fc0437ed5f0a16c57
Stephen H <stephen210huang@hotmail.com>
2017-01-17 16:04:55 -0500

    add new item complete

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,35 @@
 grocery_list = ["carrots", "apples", "napkins", "juice", "salmon"]
 
-grocery_list.each do |grocery_list|
-  puts "* #{grocery_list}"
+def print_groc(grocery_list)
+  grocery_list.each do |grocery_list|
+    puts "* #{grocery_list}"
+  end
+end
+
+
+def add_groc(new_item, grocery_list)
+  grocery_list << new_item
+  return grocery_list
+end
+
+grocery_list = add_groc("rice", grocery_list)
+print_groc(grocery_list)
+
+def count_list(grocery_list)
+  grocery_list.count
+end
+
+
+def sec_item(grocery_list)
+  puts grocery_list[1]
+end
+
+def sort_list(grocery_list)
+  sort_grocery = grocery_list.sort
+  puts "* #{sort_grocery}"
+end
+
+def rem_list(grocery_list)
+  new_list = grocery_list.pop
+  puts "* #{new_list}"
 end
