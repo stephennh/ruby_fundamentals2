59049714a23f105758b7ce95bf2d14164aca43fa
Stephen H <stephen210huang@hotmail.com>
2017-01-17 17:38:56 -0500

    exercise7.rb complete

--- exercise7.rb
+++ exercise7.rb
@@ -1,9 +1,31 @@
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
-  :cohort3 => 22
+  :cohort3 => 22,
+  :cohort4 => 43
 }
 
+# 2
 students.each do |k, v|
-  puts "#{k}: #{v}"
+  puts "#{k}: #{v} students"
 end
+
+# 4
+puts students.keys
+
+stud_value = students.values
+stud_value.each do |new_stud|
+  x = new_stud * 1.05
+  puts "#{x.to_i} is the expanded classroom"
+end
+
+# counter starts at 0
+counter = 0
+# sets a key for each students values
+stud_value = students.values
+# for each student values add it to counter
+stud_value.each do |total_stud|
+  counter += total_stud
+end
+# output the total 
+puts counter
